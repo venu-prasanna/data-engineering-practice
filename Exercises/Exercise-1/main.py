6af8d226557d51a3f32667c310262a8565ad5e61
venu-prasanna <prasanna.venu2020@gmail.com>
2023-02-12 07:21:54 +0530

    Add thread pool executor to download files

--- Exercises/Exercise-1/main.py
+++ Exercises/Exercise-1/main.py
@@ -1,4 +1,7 @@
-import requests
+import concurrent.futures
+import io
+import os
+import zipfile
 
 download_uris = [
     'https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2018_Q4.zip',
@@ -9,11 +12,34 @@ download_uris = [
     'https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2020_Q1.zip',
     'https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2220_Q1.zip'
 ]
+import requests
+
+DOWNLOAD_FOLDER_NAME = "downloads"
+
+
+def extract_zip(content):
+    zip_file = zipfile.ZipFile(io.BytesIO(content))
+    print("Extracting content...")
+    zip_file.extractall(DOWNLOAD_FOLDER_NAME)
+
+
+def download_file(url):
+    response = requests.get(url)
+    print("Downloading: ", url)
+
+    if response.status_code != 200:
+        print("failed to download ", response)
+        return
+
+    extract_zip(response.content)
 
 
 def main():
-    # your code here
-    pass
+    if not os.path.isdir(DOWNLOAD_FOLDER_NAME):
+        os.makedirs(DOWNLOAD_FOLDER_NAME)
+
+    with concurrent.futures.ThreadPoolExecutor(3) as executor:
+        executor.map(download_file, download_uris)
 
 
 if __name__ == '__main__':
